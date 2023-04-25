c976d070bf0ce81c501d60fc311f5dc2e1862e6a
natapage <104970616+natapage@users.noreply.github.com>
2023-04-25 13:42:35 +0300

    composition

--- src/App.vue
+++ src/App.vue
@@ -24,63 +24,48 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'Vue';
 import checkoutQueue from './components/CheckoutQueue.vue';
 
-export default {
-  data() {
-    return {
-      queues: [[2, 3, 5], [2, 6, 3], [1], [11, 3, 6], []],
-      newPurchasesQuantity: '',
-      isCorrectQuantity: true
-    };
-  },
-  name: 'App',
-  components: {
-    checkoutQueue,
-  },
-  methods: {
-    addCustomer() {
-      this.newPurchasesQuantity = Number(this.newPurchasesQuantity);
-      if (
-        this.newPurchasesQuantity > 0 &&
-        Number.isInteger(this.newPurchasesQuantity)
-      ) {
-        this.isCorrectQuantity = true;
-        this.queues[this.shortestQueueIndex].push(
-          this.newPurchasesQuantity);
-        this.newPurchasesQuantity = '';
-        return
-        }
-      this.isCorrectQuantity = false;
-      this.newPurchasesQuantity = '';
-    },
-    updateQueues() {
-      this.queues.forEach((queue) => {
-        if (queue.length === 0) {
-          return;
-        }
-        if (queue[0] === 1) {
-          queue.shift();
-          return;
-        }
-        queue[0] -= 1;
-      });
-    },
-  },
-  mounted() {
-    setInterval(this.updateQueues, 2000);
-  },
-  computed: {
-    shortestQueueIndex() {
-      return this.queues
-        .map((array, index) => {
-          return array.reduce((acc, item) => acc + item, 0);
-        })
-        .reduce((acc, item, index, arr) => (item < arr[acc] ? index : acc), 0);
-    },
-  },
-};
+const queues = ref([[2, 3, 5], [2, 6, 3], [1], [11, 3, 6], []]);
+const newPurchasesQuantity = ref('');
+const isCorrectQuantity = ref(true);
+
+function addCustomer() {
+  newPurchasesQuantity = Number(newPurchasesQuantity);
+  if (newPurchasesQuantity > 0 && Number.isInteger(newPurchasesQuantity)) {
+    isCorrectQuantity = true;
+    queues[shortestQueueIndex].push(newPurchasesQuantity);
+    newPurchasesQuantity = '';
+    return;
+  }
+  isCorrectQuantity = false;
+  newPurchasesQuantity = '';
+}
+
+function updateQueues() {
+  queues.forEach((queue) => {
+    if (queue.length === 0) {
+      return;
+    }
+    if (queue[0] === 1) {
+      queue.shift();
+      return;
+    }
+    queue[0] -= 1;
+  });
+}
+
+onMounted(() => setInterval(updateQueues, 2000));
+
+const shortestQueueIndex = computed(() => {
+  return queues
+    .map((array, index) => {
+      return array.reduce((acc, item) => acc + item, 0);
+    })
+    .reduce((acc, item, index, arr) => (item < arr[acc] ? index : acc), 0);
+});
 </script>
 
 <style>
